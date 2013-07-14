cb8d96e6c580f4776fc9f38453b2092afdc38968
kf <7kfpun@gmail.com>
2013-07-14 22:16:26 +0800

    add jqBootstrapValidation

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,11 @@
 $('#contact_form').submit(function() {
   $.ajax({
-    url: 'some-url',
-    type: 'post',
+    url: $(this).attr('action'),
+    type: $(this).attr('method'),
     dataType: 'json',
     data: $(this).serialize(),
     success: function(data) {
+      alert('We get your message!');
     }
   });
   return false;

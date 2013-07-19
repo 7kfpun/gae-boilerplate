c564da189b6765b9b8cc9c7d686c01c763a997f9
kf <7kfpun@gmail.com>
2013-07-19 19:32:16 +0800

    add form function

--- handlers/views.py
+++ handlers/views.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from forms import ContactForm
+from google.appengine.api import users
+from models import Contact
+
 import logging
 
 logger = logging.getLogger(__name__)
@@ -12,17 +18,25 @@ class HelloHandler(BaseHandler):
 
 class DefaultHandler(BaseHandler):
     def get(self):
-        config = self.app.config
-        self.render_response('default.html')
+        params = {
+            'form': ContactForm(self),
+        }
+        self.render_response('default.html', **params)
+
+    def post(self):
+        form = ContactForm(self)
+        params = {
+            'form': form,
+        }
+        self.response.write(form.validate())
+        self.render_response('default.html', **params)
 
 
 class StarterHandler(BaseHandler):
     def get(self):
-        config = self.app.config
         self.render_response('starter.html')
 
 
 class SigninHandler(BaseHandler):
     def get(self):
-        config = self.app.config
         self.render_response('signin.html')

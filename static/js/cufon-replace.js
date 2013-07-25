51760ab1e44a3b3d942fa090a0fc065b1a658db2
kf <7kfpun@gmail.com>
2013-07-26 00:41:26 +0800

    install emotion template

---
+++ static/js/cufon-replace.js
@@ -0,0 +1,5 @@
+Cufon.replace('.soc-ico a,footer .privacy a,footer .privacy span', { fontFamily: 'Bebas', hover:{} });
+Cufon.replace('nav>ul>li>a', { fontFamily: 'Bebas', hover:{color:'#000'} });
+Cufon.replace('h2', { fontFamily: 'Bebas'});
+
+Cufon.replace('nav>ul>li ul>li>a', { fontFamily: 'Bebas', color:'#fff'})

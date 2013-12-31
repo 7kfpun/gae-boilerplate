cd3cb61c38767c1fe3e669ecd5a3623b0cf126db
kf <7kfpun@gmail.com>
2013-12-31 12:16:13 +0700

    update urls

--- urls.py
+++ urls.py
@@ -7,11 +7,10 @@ from handlers import *
 #This is the place where all of your URL mapping goes
 urls_list = [
     (r'^/hello/$', HelloHandler),
-    #(r'^/?(.*)[/]', HomeHandler),
-    (r'^/', HomeHandler),
+    (r'^/$', HomeHandler),
 
-    (r'^/starter/', StarterHandler),
-    #(r'^/signin/', SigninHandler),
+    (r'^/starter/$', StarterHandler),
+    #(r'^/signin/$', SigninHandler),
     (r'^/contact/$', ContactHandler),
     (r'^/sitemap.xml$', SitemapHandler),
 ]
